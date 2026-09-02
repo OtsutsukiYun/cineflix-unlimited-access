096e3c83b8dadbce3907259337f7a2ce691de143
Samu <samu@cineflix.com>
2026-09-02 00:03:21 -0300

    style: remover texto secundario da secao de TV na pagina de instalacao

--- src/routes/instalar.tsx
+++ src/routes/instalar.tsx
@@ -350,7 +350,6 @@ function InstalarPage() {
                     <h2 className="text-base sm:text-lg font-black text-white">
                       Smart TV, TV Box, Fire Stick, Xiaomi Stick &amp; Projetor
                     </h2>
-                    <p className="text-xs text-white/60">Xiaomi Stick, Fire Stick, Smart TV Android, TV Box e Projetor Android</p>
                   </div>
                 </div>
 
