6f1b3c0edfef1e5c6feaec3331c6cb1a61dfcfe2
Samu <samu@cineflix.com>
2026-09-03 17:38:30 -0300

    feat: intercalar grandes lancamentos de terror 2026 com os lancamentos de acao, animacao e doramas no carrossel de fundo

--- src/routes/instalar.tsx
+++ src/routes/instalar.tsx
@@ -56,48 +56,48 @@ export const Route = createFileRoute("/instalar")({
   component: InstalarPage,
 });
 
-// LISTA DIVERSIFICADA DE FILMES 2026, SÉRIES, ANIMES E DORAMAS PARA A ESTEIRA DO FUNDO
+// LISTA DIVERSIFICADA (AÇÃO, FICÇÃO, TERROR 2026, ANIMAÇÃO, DORAMAS E ANIMES) PARA A ESTEIRA DO FUNDO
 const CATALOG_2026_POSTERS = [
   "/7GV5rrUJf0BRUhoh2cyFoeNthlQ.jpg", // Star Wars: O Mandaloriano e Grogu (2026)
+  "/wUc6IDf5ChjM1UyQye21qFBeJY0.jpg", // Obsessão (Terror 2026)
   "/360qdtu2hLnqMu8SVHMywn420w1.jpg", // Batman Knightfall (DC 2026)
-  "/cWAVzTWm9xdc8skHH7h1vreUtcD.jpg", // Motor City (2026)
-  "/gVZgjKIsXZOT3cNZm5PJZBtQRaG.jpg", // Código Vingança (2026)
+  "/uRxrNXQWkHoENm3nwVOZDYSCx2F.jpg", // Evil Dead Burn (Terror 2026)
+  "/cWAVzTWm9xdc8skHH7h1vreUtcD.jpg", // Motor City (Ação 2026)
+  "/ju10W5gl3PPK3b7TjEmVOZap51I.jpg", // Terrifier 3 (Terror)
+  "/gVZgjKIsXZOT3cNZm5PJZBtQRaG.jpg", // Código Vingança (Ação 2026)
+  "/ht8Uv9QPv9y7K0RvUyJIaXOZTfd.jpg", // Smile 2 (Terror)
   "/gpC7h43xPMEV3goYMQShfJbTtLq.jpg", // Lanternas (DC 2026)
+  "/2uSWRTtCG336nuBiG8jOTEUKSy8.jpg", // Alien Romulus (Terror Sci-Fi)
   "/gMYZZvnkVNTqSVnVCphWbPXwWwb.jpg", // Silo (Apple TV+)
+  "/uYJvxMWMb9W4zIY3cbM50sj3dpC.jpg", // The Substance (Terror Thriller)
   "/bh2OuKvq19jBHsloUVCfPSZZw81.jpg", // Vingadores: Doutor Destino (2026)
-  "/xGvz7nlGQeePcVOpAzOcHsC7kRt.jpg", // Moana 2
-  "/wHQrLuXFGiXAYyRWiYTRlDZ8T5u.jpg", // Meu Malvado Favorito 4
-  "/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg", // Deadpool & Wolverine
+  "/1EwNyiiNFd863H4e8nWEzutnZD7.jpg", // Longlegs (Terror)
+  "/xGvz7nlGQeePcVOpAzOcHsC7kRt.jpg", // Moana 2 (Animação)
+  "/5qGIxdEO841C0tdY8vOdLoRVrr0.jpg", // Nosferatu (Terror 2026)
+  "/wHQrLuXFGiXAYyRWiYTRlDZ8T5u.jpg", // Meu Malvado Favorito 4 (Animação)
+  "/v0Ljeti537c6cNKweuEN0iaU3x4.jpg", // Pecadores (Terror 2026)
+  "/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg", // Deadpool & Wolverine (Marvel)
+  "/40nHGUfypLhlr7gJx8At1IbYkaK.jpg", // Invocação do Mal 4 (Terror 2026)
   "/50yWyY981TyUHhoxxSEKwO70FmQ.jpg", // O Diabo Veste Prada 2 (2026)
+  "/p3epSUdF9qSWWHTBlA3mJ0w2i2Y.jpg", // O Telefone Preto 2 (Terror 2026)
   "/rYLQbyIvbEd0lF84iXrx7CbPcBB.jpg", // My Royal Nemesis (Dorama 2026)
-  "/4RuJf3ufe8DgQVycdyMZrJHGK1s.jpg", // Demon Slayer
-  "/geCRueV3ElhRTr0xtJuEWJt6dJ1.jpg", // Solo Leveling
-  "/9ltisibeD4gzqjM1AzmQwCdyirQ.jpg", // One Piece
+  "/temIXpcua7j5v4FipOxmzTfrB06.jpg", // Premonição 6 (Terror 2026)
+  "/4RuJf3ufe8DgQVycdyMZrJHGK1s.jpg", // Demon Slayer (Anime)
+  "/12H82Xrr2ijDF0lJWUarqGFV7bC.jpg", // Five Nights at Freddy's 2 (Terror 2026)
+  "/geCRueV3ElhRTr0xtJuEWJt6dJ1.jpg", // Solo Leveling (Anime)
+  "/skwydfnpaQdRQZfXMroh59FMJyY.jpg", // Rua do Medo Rainha do Baile (Terror 2026)
+  "/9ltisibeD4gzqjM1AzmQwCdyirQ.jpg", // One Piece (Anime)
+  "/2jME1L29XGE3T4f0zUHgpiKsPrV.jpg", // O Macaco (Terror 2026)
   "/xnxxrEKtBaIcI1ewq50pLkOMU6u.jpg", // Fúria (Furious 2026)
-  "/1C2qbfUW3lTzb8vpZeG8pjYzW3Q.jpg", // Ponto Sem Retorno (2026)
-  "/p73G56bJGPa5y52cyqWscHR6NnN.jpg", // A Queda 2: No Limite (2026)
+  "/kNxRgcTeqeU5jauBackTERoO2De.jpg", // Other Mommy (Terror 2026)
+  "/1C2qbfUW3lTzb8vpZeG8pjYzW3Q.jpg", // Ponto Sem Retorno (Ação 2026)
+  "/e0WaDBrrBAMcq2stAXCR7rXEsiw.jpg", // Just Play Dead (Terror 2026)
   "/mxa7YxVln3Rwnd2Va82PoclTznj.jpg", // Agente Kim: Reativado (Dorama 2026)
-  "/w8yyntTxZlDlZ2TR8kb4C92pmcO.jpg", // Liar Game (Anime 2026)
-  "/78m1Tv3suHmUryTI9VNPwKLKjHZ.jpg", // Witch Hat Atelier (Anime 2026)
+  "/zP83bIkBViw5b1s9bDemYJ3AAgX.jpg", // Resident Evil: O Retorno (Terror Sci-Fi 2026)
   "/yfYohBszGqoAW8oM0qydOtJ4kPh.jpg", // A Leste do Palácio (Dorama 2026)
-  "/pmff1wjKrgJi92PPr346lAifzlg.jpg", // Dia D (Disclosure Day 2026)
-  "/8hZqZB4DAC1Ebzbk80zc0H80SNa.jpg", // The Art of Sarah (Dorama 2026)
-  "/wUc6IDf5ChjM1UyQye21qFBeJY0.jpg", // Obsessão (2026)
-  "/uRxrNXQWkHoENm3nwVOZDYSCx2F.jpg", // Evil Dead Burn (2026)
-  "/kNxRgcTeqeU5jauBackTERoO2De.jpg", // Other Mommy (2026)
-  "/e0WaDBrrBAMcq2stAXCR7rXEsiw.jpg", // Just Play Dead (2026)
-  "/2sOEJzhPzjTkZSlPbGxOJ7xgIyS.jpg", // Passageiro do Mal (2026)
-  "/zP83bIkBViw5b1s9bDemYJ3AAgX.jpg", // Resident Evil: O Retorno (2026)
   "/imnkSt4PSQpxIuyiRpJCiLk3SZz.jpg", // Werwulf (O Lobisomem 2026)
-  "/c987gxFjXqYOxZEZKcTkS1ONTWH.jpg", // O Sorveteiro (2026)
-  "/5Cz1EJOKpZ8hqvBNZYF9C2zTB2T.jpg", // Acampamento Miasma (2026)
-  "/tENVMxKvOkslv8kagVNdIEQ13RT.jpg", // Cuidado com Boiúna (2026)
-  "/5jCpQnWPikggmQZoDp1eAi6BI6w.jpg", // Clayface (2026)
-  "/pRtJagIxpfODzzb0T0NAvZSzErC.jpg", // FROM (Origem)
-  "/o2Sk7VEZpR5WOCjtjO6ClGBKSji.jpg", // Revenant (O Diabo)
-  "/9DugI1HXkRAHQsjVMDnyTT8hQZx.jpg", // Ghost in the Cell (2026)
-  "/tN799oUR0f1gUKDYdMNrDaY7I51.jpg", // Colony (2026)
-  "/5M2dI8TJeRNY3Aeidhp3Ujrb3aI.jpg", // Leviticus (2026)
+  "/pmff1wjKrgJi92PPr346lAifzlg.jpg", // Dia D (Disclosure Day 2026)
+  "/pRtJagIxpfODzzb0T0NAvZSzErC.jpg", // FROM (Origem / Série de Terror)
 ];
 
 const linha1 = CATALOG_2026_POSTERS.slice(0, 8);
